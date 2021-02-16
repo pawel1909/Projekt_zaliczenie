2857906c51f371b841b3e4f3c36fb44db10137cb
pawel1909 <61648176+pawel1909@users.noreply.github.com>
2021-02-16 01:55:30 +0100

    Add project files.

---
+++ Projekt_zaliczenie/Enum/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfProject.Enum
+{
+    public enum Country
+    {
+        Poland,
+        Germany,
+        Norway,
+        Czech
+    }
+}
---
+++ Projekt_zaliczenie/Pages/Add.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfProject.Enum;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WpfProject.Pages
+{
+    /// <summary>
+    /// Interaction logic for Add.xaml
+    /// </summary>
+    public partial class Add : UserControl
+    {
+        public Add()
+        {
+            InitializeComponent();
+
+            foreach (var item in Country.GetValues(typeof(Country)))
+            {
+                Country_box.Items.Add(item);
+            }
+        }
+        /// <summary>
+        /// Dodanie danych do bazy
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Add_btn(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var eMailValidator = new System.Net.Mail.MailAddress(Mail.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Podany Email jest nieprawidłowy. Spróbuj ponownie");
+                Mail.Text = "";
+            }
+        }
+        /// <summary>
+        /// Kod ograniczający ilość cyfr i format numeru w zaleźności od wybranego kraju
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Phone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            #region Formatowanie numeru ze względu na kraj
+            string[] split = Phone.Text.Split(new char[] { '-', '(', ')' });
+            StringBuilder sb = new StringBuilder();
+
+            if (Country_box.Text == "Poland")
+            {
+                Phone.MaxLength = 15;
+                if (Phone.Text.ToString().Length == 9)
+                {
+                    foreach (var item in split)
+                    {
+                        if (item.Trim() != "")
+                        {
+                            sb.Append(item);
+                        }
+                    }
+                    this.Phone.Text = String.Format("+48 {0:000 000 000}", double.Parse(sb.ToString()));
+                }
+            }
+
+            if (Country_box.Text == "Germany")
+            {
+                Phone.MaxLength = 17;
+                if (Phone.Text.ToString().Length == 11)
+                {
+                    foreach (var item in split)
+                    {
+                        if (item.Trim() != "")
+                        {
+                            sb.Append(item);
+                        }
+                    }
+                    this.Phone.Text = String.Format("+49 {0:000 000 00000}", double.Parse(sb.ToString()));
+                }
+            }
+
+            if (Country_box.Text == "Norway")
+            {
+                Phone.MaxLength = 14;
+                if (Phone.Text.ToString().Length == 9)
+                {
+                    foreach (var item in split)
+                    {
+                        if (item.Trim() != "")
+                        {
+                            sb.Append(item);
+                        }
+                    }
+                    this.Phone.Text = String.Format("+47 {0:00 00 00 00}", double.Parse(sb.ToString()));
+                }
+            }
+
+            if (Country_box.Text == "Czech")
+            {
+                Phone.MaxLength = 16;
+                if (Phone.Text.ToString().Length == 9)
+                {
+                    foreach (var item in split)
+                    {
+                        if (item.Trim() != "")
+                        {
+                            sb.Append(item);
+                        }
+                    }
+                    this.Phone.Text = String.Format("+420 {0:000 000 000}", double.Parse(sb.ToString()));
+                }
+            }
+            #endregion
+        }
+        /// <summary>
+        /// Czyszczenie pola na numer telefonu, po wybraniu innego kraju.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Phone.Text = "";
+        }
+        /// <summary>
+        /// Przyjmowanie tylko liczb w TextBox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Mail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Imie_nazw_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Imie_nazw.Text.Length > 2)
+            {
+                if (Imie_nazw.Text[Imie_nazw.Text.Length - 1].ToString() == " " && Imie_nazw.Text[Imie_nazw.Text.Length - 2].ToString() == " ")
+                {
+                    string[] spl = Imie_nazw.Text.Split(' ');
+                    string temp = "";
+
+                    for (int i = 0; i < spl.Length; i++)
+                    {
+                        temp += spl[i];
+                    }
+
+                    Imie_nazw.Text = temp;
+                }
+            }
+        }
+    }
+}
